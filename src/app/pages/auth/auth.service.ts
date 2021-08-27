769bcb87807ecc8e40376756e9c41c9ceb3ede19
shalithadeshan <shalithad8520jayasekara@gmail.com>
2021-08-27 23:20:23 +0530

    learn ion grid

--- src/app/pages/auth/auth.service.ts
+++ src/app/pages/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AuthService {
-  private userIsAuthenticated = false;
+  private userIsAuthenticated = true;
 
   get userIsAuthenticatedMethod() {
     return this.userIsAuthenticated;
